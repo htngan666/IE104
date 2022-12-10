1333575e6081296242148f2bbc9cc62579bf6a01
Huy010302 <111289985+Huy010302@users.noreply.github.com>
2022-12-11 03:02:47 +0700

    QuanLy

---
+++ QuanLy/assets/script/navbar.js
@@ -0,0 +1,9 @@
+/** @format */
+
+const btnMore = document.querySelector(".showNav span");
+const more = document.querySelector(".gap");
+btnMore.addEventListener("click", function () {
+  if (document.querySelector(".hide-mobile"))
+    more = more.classList.remove("hide-mobile");
+  else more = more.classList.add("hide-mobile");
+});
