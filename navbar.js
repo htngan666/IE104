4be233c51deab44ee36ba6c17b968303a88870de
Bao Gia Bảo <64829950+IllusID@users.noreply.github.com>
2022-12-11 13:23:28 +0700

    Fixed Directories

--- navbar.js
+++
@@ -1,6 +0,0 @@
-const btnMore = document.querySelector(".showNav span");
-const more = document.querySelector(".gap");
-btnMore.addEventListener("click", function () {
-    if (document.querySelector(".hide-mobile")) more = more.classList.remove("hide-mobile");
-    else more = more.classList.add("hide-mobile");
-});
